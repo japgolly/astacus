5d02334edaab9ad05006b817adb865e24ec9036e
David Barri <japgolly@gmail.com>
2009-06-16 22:21:29 +1000

    MySQL DB creation SQL

---
+++ db/mysql_db_creation.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE astacus_test CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+GRANT ALL ON astacus_test.* TO 'astacus_test'@'localhost' IDENTIFIED BY 'astacus_test';
+FLUSH PRIVILEGES;
+
+CREATE DATABASE astacus_dev CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+GRANT ALL ON astacus_dev.* TO 'astacus_dev'@'localhost' IDENTIFIED BY 'astacus_dev';
+FLUSH PRIVILEGES;
+
