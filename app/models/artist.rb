889b9567e87be279b306562c55450a7e758604f0
David Barri <japgolly@gmail.com>
2009-07-04 22:48:08 +1000

    artist name uniqueness no longer case sensitive

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,6 +1,6 @@
 class Artist < ActiveRecord::Base
   has_many :albums
   validates_presence_of :name
-  validates_uniqueness_of :name, :case_sensitive => false, :allow_blank => false, :allow_nil => true
+  validates_uniqueness_of :name, :allow_blank => false, :allow_nil => true
   acts_as_unique
 end
--- test/unit/artist_test.rb
+++ test/unit/artist_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class ArtistTest < ActiveSupport::TestCase
   should_have_many :albums
   should_validate_presence_of :name
-  should_validate_uniqueness_of :name, :case_sensitive => false
+  should_validate_uniqueness_of :name, :case_sensitive => true
   should_not_allow_values_for :name, '', '   ', :message => "can't be blank"
 
   context "Artists" do
