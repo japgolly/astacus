4c78ba54fd79f8fefdbaa03d2e2aa1563fccc016
David Barri <japgolly@gmail.com>
2009-07-15 01:04:19 +1000

    Location dirs are now absolute and converted from win->cygwin

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,4 +2,11 @@ class Location < ActiveRecord::Base
   attr_readonly :dir
   validates_presence_of :dir, :label
   validates_uniqueness_of :dir
+
+  def before_validation
+    unless dir.blank?
+      self.dir= `cygpath -au #{dir.inspect}`.chomp if RUBY_PLATFORM =~ /cygwin/i
+      self.dir= File.expand_path(dir)
+    end
+  end
 end
--- test/unit/location_test.rb
+++ test/unit/location_test.rb
@@ -4,5 +4,28 @@ class LocationTest < ActiveSupport::TestCase
   should_validate_presence_of :label
   should_validate_presence_of :dir
   should_have_readonly_attributes :dir
-  should_validate_uniqueness_of :dir
+
+  context "Locations" do
+    should "have unique dirs" do
+      assert_difference 'Location.count' do
+        Location.create(:label => 'asd', :dir => File.dirname(__FILE__))
+      end
+      assert_no_difference 'Location.count' do
+        Location.create(:label => 'omg', :dir => File.dirname(__FILE__))
+      end
+    end
+
+    should "use canonical pathnames" do
+      dir= File.join(File.dirname(__FILE__),".",".",".")
+      Location.create(:label => 'asd', :dir => dir)
+      assert_equal File.expand_path(dir), Location.last.dir
+    end
+
+    if RUBY_PLATFORM =~ /cygwin/i
+      should "translate windows paths into cygwin paths" do
+      Location.create(:label => 'asd', :dir => "c:\\blah\\")
+      assert_match %r!^(/cygdrive)?/c/blah$!, Location.last.dir
+      end
+    end
+  end
 end
