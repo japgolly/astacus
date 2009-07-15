1fbc7f33ba6a6f07c6d5db6486d60a684a1fd39c
David Barri <japgolly@gmail.com>
2009-07-16 02:20:16 +1000

    Added a helper model_errors_to_single_string()

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,6 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def model_errors_to_single_string(model)
+    model.errors.map{|a| "#{a[0].capitalize} #{a[1]}."}.join("\n\n")
+  end
 end
