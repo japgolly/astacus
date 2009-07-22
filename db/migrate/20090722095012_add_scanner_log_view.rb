5f1da2c6e1ce844fd97ad6f56c958c431e29a33a
David Barri <japgolly@gmail.com>
2009-07-23 02:30:58 +1000

    Modified scanner log view

--- db/migrate/20090722095012_add_scanner_log_view.rb
+++ db/migrate/20090722095012_add_scanner_log_view.rb
@@ -3,8 +3,11 @@ class AddScannerLogView < ActiveRecord::Migration
     execute <<-EOS
       create view v_scanner_logs as
       select sl.*
-        , round((case when ended is null then utc_timestamp() else ended end)-started,1) duration
-        , case when files_scanned is null then 0 else round(files_scanned/((case when ended is null then utc_timestamp() else ended end)-started)*3600) end fph
+        , round(timestampdiff(second,started,case when ended is null then utc_timestamp() else ended end)/60,1) duration_min
+        , round(timestampdiff(second,started,case when ended is null then utc_timestamp() else ended end)/3600,2) duration_hr
+        , case when files_scanned is null then 0
+          else round(files_scanned*3600/timestampdiff(second,started,case when ended is null then utc_timestamp() else ended end))
+          end fph
         , l.dir
       from scanner_logs sl, locations l
       where sl.location_id=l.id;
