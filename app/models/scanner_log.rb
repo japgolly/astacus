cf983aaac7719830ae1701899d52252a26478192
David Barri <japgolly@gmail.com>
2009-07-23 01:59:10 +1000

    Added an ETA to the location view

--- app/models/scanner_log.rb
+++ app/models/scanner_log.rb
@@ -4,4 +4,10 @@ class ScannerLog < ActiveRecord::Base
   validates_inclusion_of :active, :in => [true, false]
   validates_numericality_of :files_scanned, :only_integer => true, :greater_than_or_equal_to => 0, :allow_nil => true
   validates_numericality_of :file_count, :only_integer => true, :greater_than_or_equal_to => 0, :allow_nil => true
+
+  def time_remaining
+    return :na unless active?
+    return :unknown unless files_scanned and files_scanned > 0
+    ((Time.now - started)/files_scanned * (file_count - files_scanned)).seconds
+  end
 end
