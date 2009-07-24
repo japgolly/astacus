09395c5923aba13bbe43f3d09e89968c7b9a6531
David Barri <japgolly@gmail.com>
2009-07-24 19:44:56 +1000

    Added a performance test for finding audio_tags by audio_files. Fixed by adding indicies.

---
+++ test/performance/performance_test_helper.rb
@@ -0,0 +1,66 @@
+require 'test_helper'
+
+class PerformanceTest < ActiveSupport::TestCase
+  self.fixture_path = File.join(File.dirname(__FILE__), "../fixtures/performance" )
+  MINIMUM_ROWS= 1000
+
+  def self.test_find_identical(model, reps, time)
+    test_name= model.to_s.underscore
+    class_eval <<-EOB
+      def test_#{test_name}_new
+        assert #{model}.count >= MINIMUM_ROWS, "Fixture too small. #{model}.count = \#{#{model}.count}."
+        m= #{model}.first
+        #{model}.delete m.id
+        m= m.clone
+        silence #{model} do
+          elapsed_time= Benchmark.realtime do
+            #{reps}.times {
+              assert_nil m.find_identical
+            }
+          end
+          assert_time_within #{time}, elapsed_time
+        end
+      end
+
+      def test_#{test_name}_existing
+        m= #{model}.first
+        m= m.clone
+        silence #{model} do
+          elapsed_time= Benchmark.realtime do
+            #{reps}.times {
+              assert_not_nil m.find_identical
+            }
+          end
+          assert_time_within #{time}, elapsed_time
+        end
+      end
+    EOB
+  end
+
+  PROCS= {}
+  def self.test_find(model, name, reps, time, &condition_proc)
+    test_name= "test_#{model.to_s.underscore}_find_#{name.to_s.underscore.gsub /\s/,'_'}"
+    PROCS[test_name]= condition_proc
+    class_eval <<-EOB
+      def #{test_name}
+        assert #{model}.count >= MINIMUM_ROWS, "Fixture too small. #{model}.count = \#{#{model}.count}."
+        silence #{model} do
+          elapsed_time= Benchmark.realtime do
+            #{reps}.times {
+              #{model}.find :all, :conditions => PROCS['#{test_name}'].call(self)
+            }
+          end
+          assert_time_within #{time}, elapsed_time
+        end
+      end
+    EOB
+  end
+
+  def silence(base = ActiveRecord::Base)
+    base.logger ? base.logger.silence{yield} : yield
+  end
+
+  def assert_time_within(max, actual_time)
+    assert actual_time < max, "Too slow. Actual time was %.2f which is above #{max}." % [actual_time]
+  end
+end
