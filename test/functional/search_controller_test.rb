32968f1f2ef905aa1f6c8dcc42464be14086db91
David Barri <japgolly@gmail.com>
2009-08-03 00:00:05 +1000

    Added a test for search/search with no params

--- test/functional/search_controller_test.rb
+++ test/functional/search_controller_test.rb
@@ -1,8 +1,23 @@
 require 'test_helper'
 
 class SearchControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+
+  context "Search without any params" do
+    setup {get :search}
+    should_respond_with :success
+    should_render_template :search
+    should_assign_to :albums, :class => WillPaginate::Collection
+    should "render album data" do
+      assert_select '.albumart img'
+      assert_response_includes 'Disc 1'
+      assert_response_includes 'Disc 2'
+      assert_response_includes 'Disc 3'
+      assert_response_includes 'Still Life'
+      assert_response_includes 'A Pleasant Shade of Gray XII'
+      assert_response_includes 'The Eleventh Hour'
+      assert_response_includes '192 kbps'
+      assert_response_includes 'The Sound Of Muzak'
+      assert_response_includes '4:59' # length of The Sound Of Muzak
+    end
   end
 end
