9797b4976571fda0f0314706de1a8d06dc6a9e33
David Barri <japgolly@gmail.com>
2009-08-03 00:31:03 +1000

    search now filters on artist

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,14 +1,37 @@
 class SearchController < ApplicationController
   def search
-    @albums= Album.paginate({
+    # Default options
+    options= {
       :page => params[:page] || 1,
       :per_page => 10,
-      :joins => :artist,
       :include => [:artist, {:discs => {:tracks => {:audio_file => :audio_content}}}],
       :readonly => true,
-      :order => 'artists.name, albums.year, albums.name',
-#      :conditions => 'albums.id in(37,38)',
-    })
+    }
+
+    # Filter options
+    filter_options= {}
+    if params[:artist]
+      add_to_array_param filter_options, :joins, :artist
+      filter_options[:conditions]= ['upper(artists.name) LIKE upper(?)', "%#{params[:artist]}%"]
+    end
+    options.deep_merge! filter_options
+
+    # Sort options
+    add_to_array_param options, :joins, :artist
+    options.merge! :order => 'artists.name, albums.year, albums.name'
+
+    # Get results
+    @albums= Album.paginate(options)
   end
+
+  private
+    def add_to_array_param(hash, key, value)
+      if hash[key]
+        hash[key]= [hash[key]] unless hash[key].is_a?(Array)
+        hash[key]<< value unless hash[key].include?(value)
+      else
+        hash[key]= [value]
+      end
+    end
 
 end
--- test/functional/search_controller_test.rb
+++ test/functional/search_controller_test.rb
@@ -2,11 +2,20 @@ require 'test_helper'
 
 class SearchControllerTest < ActionController::TestCase
 
+  def self.should_pass_common_assertions
+    class_eval <<-EOB
+      should_respond_with :success
+      should_render_template :search
+      should_assign_to :albums, :class => WillPaginate::Collection
+    EOB
+  end
+
   context "Search without any params" do
     setup {get :search}
-    should_respond_with :success
-    should_render_template :search
-    should_assign_to :albums, :class => WillPaginate::Collection
+    should_pass_common_assertions
+    should "contain all albums" do
+      assert_same_elements Album.find(:all), assigns(:albums)
+    end
     should "render album data" do
       assert_select '.albumart img'
       assert_response_includes 'Disc 1'
@@ -18,6 +27,14 @@ class SearchControllerTest < ActionController::TestCase
       assert_response_includes '192 kbps'
       assert_response_includes 'The Sound Of Muzak'
       assert_response_includes '4:59' # length of The Sound Of Muzak
+    end
+  end
+
+  context "Search filtered by artist" do
+    setup {get :search, :artist => 'RCupIN'}
+    should_pass_common_assertions
+    should "filter its results appropriately" do
+      assert_same_elements artists(:porcupine_tree).albums, assigns(:albums)
     end
   end
 end
