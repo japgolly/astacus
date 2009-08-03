4b3b4184736ec59fb2ab6bb335325063132e7474
David Barri <japgolly@gmail.com>
2009-08-03 21:40:41 +1000

    Fixed a bug in all_models() that was causing models to sometimes be loaded twice (which caused other horrible bugs)

--- lib/rails_reflection.rb
+++ lib/rails_reflection.rb
@@ -2,7 +2,10 @@ module RailsReflection
 
   def all_models
     @@all_models||= (
-      Dir.glob('app/models/**/*.rb').each{|f| require f}
+      Dir.glob('app/models/**/*.rb').each{|f|
+        model_name= f.gsub(/^.+[\\\/]|\.rb$/,'').camelize
+        model_name.constantize rescue require f
+      }
       Object.constants.map{|c| eval c}.select{|c| c.respond_to? :table_name}
     )
   end
