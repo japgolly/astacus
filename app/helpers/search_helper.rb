fbb4a2ed29538c694992ace238e610a6e0416e5e
David Barri <japgolly@gmail.com>
2009-08-04 20:57:18 +1000

    Search query form now looks prurrrdy

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -9,28 +9,35 @@ module SearchHelper
     %!<td class="#{cell_class}">&nbsp;</td>! unless i == 0
   end
 
+  def sq_field_group(title,&block)
+    content= capture(&block)
+    concat %!<table class="fields"><tr><th colspan="2">#{h title}</th></tr>!
+    concat(content)
+    concat "</table>"
+  end
+
   def sq_generic_field(key, label_text, input_tag)
-    label_tag= label_tag(key, "#{label_text}:")
-    %!<div>#{label_tag} #{input_tag}</div>!
+    label_tag= label_tag(key, label_text)
+    %!<tr><td class="l">#{label_tag}</td><td class="f">#{input_tag}</td></tr>!
   end
 
   def sq_boolean_field(key, label_text, yes_label, no_label, yes_before_no= true)
     nada= ['', nil]
-    yes= ["#{yes_label}_nbsp", '1']
-    no= ["#{no_label}_nbsp", '0']
+    yes= [yes_label, '1']
+    no= [no_label, '0']
     option_array= yes_before_no ? [nada,yes,no] : [nada,no,yes]
     o= options_for_select option_array, @sq.params[key]
-    t= select_tag key, o.gsub('_nbsp','&nbsp;'), :id => nil
+    t= select_tag key, o.gsub('</option>','&nbsp;</option>')
     sq_generic_field key, label_text, t
   end
 
   def sq_int_field(key, label_text, size)
-    t= text_field_tag key, @sq.params[key], :id => nil, :size => size*2+2
+    t= text_field_tag key, @sq.params[key], :size => size*2+2
     sq_generic_field key, label_text, t
   end
 
   def sq_text_field(key, label_text)
-    t= text_field_tag key, @sq.params[key], :id => nil
+    t= text_field_tag key, @sq.params[key], :class => 'txt'
     sq_generic_field key, label_text, t
   end
 end
