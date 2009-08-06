b2fac5a3849064c5fd68290a87a63f69103dc2db
David Barri <japgolly@gmail.com>
2009-08-06 19:45:43 +1000

    Added a stat_row_percentage helper method

--- app/helpers/stats_helper.rb
+++ app/helpers/stats_helper.rb
@@ -2,4 +2,10 @@ module StatsHelper
   def stat_row(key,value)
     "<tr><td>#{key}</td><td>#{value}</td></tr>"
   end
+
+  def stat_row_percentage(key, a, b, decimal_places=0)
+    v= format(a)
+    v+= " (#{to_percentage a,b,decimal_places}%)" unless b == 0
+    stat_row key, v
+  end
 end
