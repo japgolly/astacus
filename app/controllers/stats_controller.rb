66700e799ca8cb4e3ebfb5f452a0de487b075d48
David Barri <japgolly@gmail.com>
2009-08-06 18:54:33 +1000

    Started writing a test for stats/index Gave it a revised look with stats put in categories.

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,27 +1,36 @@
 class StatsController < ApplicationController
   def index
     conn= ActiveRecord::Base.connection
-    # conn.select_value(''),
     @stats= {
       :files                    => AudioFile.count,
       :filesize                 => AudioFile.sum(:size),
-      :songs                    => -1, #xxxxxxxxx,
       :artists                  => Artist.count,
+      :non_va_artists           => Album.count(:select => 'distinct artist_id', :joins => :discs, :conditions => 'discs.va=0'),
       :albums                   => Album.count,
-      :cds                      => Cd.count,
+      :non_va_albums            => Album.count(:select => 'distinct albums.id', :joins => :discs, :conditions => 'discs.va=0'),
+      :multiple_disc_albums     => Album.count(:conditions => "discs_count>1"),
+      :discs                    => Disc.count,
       :tracks                   => Track.count,
       :length                   => AudioContent.sum(:length),
-      :avg_albums_p_artist      => conn.select_value('select avg(c) from (select count(*) c from albums group by artist_id) a;'),
-      :avg_tracks_p_artist      => conn.select_value('select avg(c) from (select count(*) c from tracks t inner join cds on cds.id=cd_id inner join albums al on al.id=album_id group by artist_id) a;'),
-      :avg_tracks_p_cd          => conn.select_value('select avg(c) from (select count(*) c from tracks group by cd_id) a;'),
-      :avg_length               => AudioContent.average(:length), # avg track length
-      :avg_filesize             => AudioFile.average(:size), # avg file size
+      :avg_tracks_p_artist      => conn.select_value('select avg(c) from (select count(*) c from tracks t inner join discs on discs.id=disc_id inner join albums al on al.id=album_id group by artist_id) a;').to_f,
       :avg_bitrate              => AudioContent.average(:bitrate), # avg bitrate
-#      :xxxxx                    => xxxxxxxxx, # albums with albumart
-#      :xxxxx                    => xxxxxxxxx, # albums without albumart
+#      :xxxxx                    => xxxxxxxxx, # discs with albumart
+#      :xxxxx                    => xxxxxxxxx, # discs without albumart
 #      :xxxxx                    => xxxxxxxxx, # bitrate buckets
 #      :xxxxx                    => xxxxxxxxx, # length buckets
 #      :xxxxx                    => xxxxxxxxx, # album year buckets
     }
+    @stats[:avg_filesize]= safe_avg(:filesize,:files)
+    @stats[:avg_length]= safe_avg(:length,:tracks)
+    @stats[:avg_albums_p_artist]= safe_avg(:non_va_albums,:non_va_artists)
+    @stats[:avg_tracks_p_disc]= safe_avg(:tracks,:discs)
   end
+
+  private
+    def safe_avg(total,count)
+      count= @stats[count] if count.is_a?(Symbol)
+      return 0 if count == 0
+      total= @stats[total] if total.is_a?(Symbol)
+      total.to_f / count
+    end
 end
--- test/functional/stats_controller_test.rb
+++ test/functional/stats_controller_test.rb
@@ -1,8 +1,80 @@
 require 'test_helper'
 
 class StatsControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
-  end
+  TOTAL_FILESIZE= \
+    19989119 + # glass_prison
+    13507820 + # misunderstood
+    8427118 + # about_to_crash
+    11494512 + # the_requiem
+    3108860 + # apsog_i
+    5179014 + # apsog_ii
+    1712044 + # apsog_x
+    4875580 + # apsog_xi
+    10834006 + # apsog_xii
+    30106094 + # the_ivory_gate_of_dreams
+    11880343 + # the_eleventh_hour
+    98897 + # blackest_eyes
+    98797 + # trains
+    99839 + # lips_of_ashes
+    101371 + # the_sound_of_muzak
+    215037 + # silsila_ye_chaahat_ka
+    9761101 + # maar_daala
+    11319985 + # bairi_piya
+    11537513 + # kaahe_chhed_mohe
+  0#end TOTAL_FILESIZE
+
+  TOTAL_LENGTH= \
+    834 + # glass_prison
+    564 + # misunderstood
+    352 + # about_to_crash
+    372 + # the_requiem
+    129.463 + # apsog_i
+    215.719 + # apsog_ii
+    71.262 + # apsog_x
+    203.076 + # apsog_xi
+    451.318 + # apsog_xii
+    1254.35 + # the_ivory_gate_of_dreams
+    494.942 + # the_eleventh_hour
+    263.654 + # blackest_eyes
+    356.467 + # trains
+    279.301 + # lips_of_ashes
+    299.154 + # the_sound_of_muzak
+    324.885 + # silsila_ye_chaahat_ka
+    278.256 + # maar_daala
+    321.907 + # bairi_piya
+    322.403 + # kaahe_chhed_mohe
+  0#end TOTAL_LENGTH
+
+  #=============================================================================
+
+  context "stats/index" do
+    context "with no params" do
+      setup do
+        AudioContent.update_all 'bitrate=10'
+        AudioContent.update audio_content(:glass_prison).id, :bitrate => 1234
+        get :index
+        @stats= assigns(:stats)
+      end
+      should_respond_with :success
+      should "calculate the stats correctly" do
+        ac_count= AudioContent.count
+        assert_equal files=19, @stats[:files]
+        assert_equal TOTAL_FILESIZE, @stats[:filesize]
+        assert_equal 8, @stats[:artists]
+        assert_equal nva_artists=4, @stats[:non_va_artists]
+        assert_equal 5, @stats[:albums]
+        assert_equal nva_albums=4, @stats[:non_va_albums]
+        assert_equal 2, @stats[:multiple_disc_albums]
+        assert_equal discs=8, @stats[:discs]
+        assert_equal tracks=19, @stats[:tracks]
+        assert_in_delta TOTAL_LENGTH, @stats[:length], 0.01
+        assert_in_delta nva_albums.to_f / nva_artists.to_f, @stats[:avg_albums_p_artist], 0.01
+#      :avg_tracks_p_artist      => conn.select_value('select avg(c) from (select count(*) c from tracks t inner join discs on discs.id=disc_id inner join albums al on al.id=album_id group by artist_id) a;').to_f,
+        assert_in_delta tracks.to_f / discs.to_f, @stats[:avg_tracks_p_disc], 0.01
+        assert_in_delta TOTAL_LENGTH / tracks, @stats[:avg_length], 0.01
+        assert_in_delta TOTAL_FILESIZE.to_f / files, @stats[:avg_filesize], 0.1
+        assert_in_delta (1234 + 10*(ac_count-1)).to_f / ac_count, @stats[:avg_bitrate], 0.1
+      end
+    end # Context: with no params
+  end # Context: stats/index
 end
