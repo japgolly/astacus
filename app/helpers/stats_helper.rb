649ae87586c61e6dfffb2fd54c480d6434d2f1c5
David Barri <japgolly@gmail.com>
2009-08-08 02:43:53 +1000

    Added dashed lines to every 10 years in the albums_by_year graph

--- app/helpers/stats_helper.rb
+++ app/helpers/stats_helper.rb
@@ -11,9 +11,11 @@ module StatsHelper
     concat(content)
   end
 
-  def stats_row(key, value, value2=nil)
+  def stats_row(key, value, value2=nil, options={})
     value2= %!<td class="v2">#{value2}</td>! if value2
-    %!<tr class="alt#{@alt^=1}"><td class="k">#{key}</td><td class="v">#{value}</td>#{value2}</tr>!
+    tr_class= "alt#{@alt^=1}"
+    tr_class+= " #{options[:tr_class]}" if options[:tr_class]
+    %!<tr class="#{tr_class}"><td class="k">#{key}</td><td class="v">#{value}</td>#{value2}</tr>!
   end
 
   def stats_row_percentage(key, a, b, decimal_places=0)
@@ -21,34 +23,39 @@ module StatsHelper
     stats_row key, v
   end
 
-  def stats_graph(id, title, &block)
+  def stats_graph(id, title, options={}, &block)
     data= @stats[id.to_sym]
     block||= method(:default_stats_label_gen).to_proc
     x= %!<table class="graph" id="#{id}">!
     x+= capture do
       stats_section(title, 3) do
-        stats_graph_body(data, &block)
+        stats_graph_body(data, options, &block)
       end
     end
     x+= '</table>'
   end
 
-  def stats_graph_body(data, &block)
+  def stats_graph_body(data, options, &block)
     step= data[:step]
+    m_lines= options[:m_lines]
     x= ''
-    x+= stats_graph_row(block.call(nil,step), data[nil], data[:max_value], 'nil') if data[nil]
+    if data[nil]
+      x+= stats_graph_row block.call(nil,step), data[nil], data[:max_value],
+        :div_class => 'nil', :tr_class => 'nil'
+    end
     i= data[:min]
     while i<= data[:max]
-      x+= stats_graph_row(block.call(i,step), data[i] || 0, data[:max_value])
+      x+= stats_graph_row block.call(i,step), data[i] || 0, data[:max_value],
+        :tr_class => (m_lines and i % m_lines ==0) ? 'm_top' : nil
       i+= step
     end
     x
   end
 
-  def stats_graph_row(title, value, max, div_class=nil)
+  def stats_graph_row(title, value, max, options={})
     p= value == 0 ? 0 : to_percentage(value,max,0)
-    div_class= %!class="#{div_class}"! if div_class
-    stats_row(title, %!<div #{div_class} style="width:#{p}%">&nbsp;</div>!, value) + "\n"
+    div_class= %! class="#{options[:div_class]}"! if options[:div_class]
+    stats_row(title, %!<div#{div_class} style="width:#{p}%">&nbsp;</div>!, value, options) + "\n"
   end
 
   def default_stats_label_gen(i, step)
