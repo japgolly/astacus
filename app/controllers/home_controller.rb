2323f63dd0fad54637d3caeab18d66c3bf582b8f
David Barri <japgolly@gmail.com>
2009-08-16 01:30:11 +1000

    Added a login form and logout button to the views

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,15 +6,20 @@ class HomeController < ApplicationController
     return redirect_to(root_url) unless request.xhr? and request.post?
     u,p = params[:username],params[:password]
     @user= User.find_by_username_and_password(u,p) rescue nil if u and p
-    session[USER_ID_SESSION_KEY]= @user.id if @user
+    set_cur_user @user.id if @user
   end
 
   def logout
-    session[USER_ID_SESSION_KEY]= nil
+    set_cur_user nil
     if request.xhr?
       render(:update) {|page| page.reload}
     else
       redirect_to root_url
     end
   end
+
+  private
+    def set_cur_user(id)
+      session[ApplicationHelper::USER_ID_SESSION_KEY]= id
+    end
 end
