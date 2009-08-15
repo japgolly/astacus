8c240dac96ec777c3e9b832d1e3333efe54112fe
David Barri <japgolly@gmail.com>
2009-08-15 13:12:49 +1000

    Added capistrano files

---
+++ config/deploy.rb
@@ -0,0 +1,34 @@
+set :application, "astacus"
+set :domain,      "vm-appserver"
+set :repository,  "ssh://#{domain}/mnt/hgfs/x/Dropbox/My Dropbox/projects/#{application}.git"
+set :use_sudo,    false
+set :deploy_to,   "/srv/#{application}"
+set :scm,         "git"
+
+# server "vm-appserver", :app, :web, :db, :primary => true
+role :app, domain
+role :web, domain
+role :db,  domain, :primary => true
+
+namespace :deploy do
+  task :start, :roles => :app do
+    run "touch #{current_release}/tmp/restart.txt"
+  end
+
+  task :stop, :roles => :app do
+    # Do nothing.
+  end
+
+  desc "Restart Application"
+  task :restart, :roles => :app do
+    run "touch #{current_release}/tmp/restart.txt"
+  end
+
+  # For deploying a database.yml file.
+  #task :after_update_code, :roles => :app do
+  #  run "ln -nfs #{deploy_to}/shared/system/database.yml #{release_path}/config/database.yml"
+  #end
+end
+
+after :deploy, "passenger:restart"
+
