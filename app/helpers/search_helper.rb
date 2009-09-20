746ab9125f02a64b8e8ac2eb9df10359bd631b86
David Barri <japgolly@gmail.com>
2009-09-20 17:15:21 +1000

    AST-1: Added info buttons next to int SQ fields

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -1,4 +1,5 @@
 module SearchHelper
+  extend ActiveSupport::Memoizable
 
   # If i is non-zero, returns
   #   <td class="abc">&nbsp;</td>
@@ -16,9 +17,12 @@ module SearchHelper
     concat "</table>"
   end
 
-  def sq_generic_field(key, label_text, input_tag)
+  def sq_generic_field(key, label_text, input_tag, options={})
     label_tag= label_tag(key, label_text)
-    %!<tr><td class="l">#{label_tag}</td><td class="f">#{input_tag}</td></tr>!
+    hint= if options[:hint]
+      " #{image_tag 'info.png', :alt => '', :class => 'hint', :onclick => %!alert("#{escape_javascript options[:hint]}")! }"
+    end
+    %!<tr><td class="l">#{label_tag}</td><td class="f">#{input_tag}#{hint}</td></tr>!
   end
 
   def sq_boolean_field(key, label_text, yes_label='Yes', no_label='No', yes_before_no= true)
@@ -31,13 +35,31 @@ module SearchHelper
     sq_generic_field key, label_text, t
   end
 
-  def sq_int_field(key, label_text, size)
+  def sq_int_field(key, label_text, size, options={})
     t= text_field_tag key, @sq.params[key], :size => size*2+2
-    sq_generic_field key, label_text, t
+    sq_generic_field key, label_text, t, options
   end
 
-  def sq_text_field(key, label_text)
+  def sq_text_field(key, label_text, options={})
     t= text_field_tag key, @sq.params[key], :class => 'txt'
-    sq_generic_field key, label_text, t
+    sq_generic_field key, label_text, t, options
   end
+
+  def generic_int_hint(name, a,b,c,d, more, less)
+    more= more.to_s.downcase
+    less= less.to_s.downcase
+    examples= []
+    examples<< ["#{a}-#{d}", "#{a}, #{d} or anything in between"]
+    examples<< ["#{b}+", "#{b} or #{more}"]
+    examples<< ["#{b}>", "#{more.titlecase} than #{b}"]
+    examples<< ["#{b}=>", "#{b} or #{more}"]
+    examples<< ["#{b}>=", "#{b} or #{more}"]
+    examples<< ["-#{d}", "#{d} or #{less}"]
+    examples<< ["<#{d}", "#{less.titlecase} than #{d}"]
+    examples<< ["<=#{d}", "#{d} or #{less}"]
+    len= examples.map{|e| e[0].length}.max
+    examples_str= examples.map{|e| "%-#{len}s      \t(%s)" % e}.join("\n")
+    %!Valid #{name} filter examples:\n#{examples_str}\n\nYou can also use any combination separated by commas. Example:\n#{a}-#{b}, #{c}, #{d}+!
+  end
+  memoize :generic_int_hint
 end
