8c5a8f6abce5b00cca48f98f80c247d3715b3fcf
David Barri <japgolly@gmail.com>
2010-12-17 22:18:09 +1100

    First pass of upgrade to Rails 3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Astacus::Application.config.secret_token = '708b56c409949584da57ca85fbeadbc89336cec1c1a6911bec1d86bee3216d59b8d1ca239e7866e2622645a97aa83ca813844229f9628d9f071cbfbd3a018979'
